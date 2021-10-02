11591cb0adf38ee4c70226608205612c45017466
Helena Soto Chaves <helena.soto.c@gmail.com>
2021-10-02 15:07:33 -0300

    Criação de Inicio

---
+++ src/components/Inicio.vue
@@ -0,0 +1,24 @@
+<template>
+  <v-container>
+    
+    <v-img
+      class="inicio-imagem mb-4"
+      src="https://www.ifmg.edu.br/betim/noticias/estudantes-indicam-atividades-de-lazer-durante-o-periodo-de-isolamento/Sitebanner.png/@@images/6a294f56-0909-4cb1-ad4a-1f4a0c532832.png">
+    </v-img>
+    <p style="text-align:justify;">
+      Lazer corresponde ao tempo de folga, passatempo, descanso, distração ou entretenimento de uma pessoa.
+    </p>        
+  </v-container>
+</template>
+
+<script>
+export default {
+  name: 'Inicio'
+}
+</script>
+
+<style scoped>
+.inicio-imagem {
+  border-radius: 3px;
+}
+</style>
