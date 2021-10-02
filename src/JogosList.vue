62a4029527439f791b5125e2f266d51a23513a03
Thiago Caldeira da Silva <thiago.caldeiraa@gmail.com>
2021-10-02 15:28:39 -0300

    Teste-Branch
    Teste.

---
+++ src/JogosList.vue
@@ -0,0 +1,30 @@
+<template>
+  <v-container>
+    <h2 class= "text-h5 text-center mb-3 mt5">Lista de melhores jogos!</h2>
+    <!-- Passando a prop para exibição -->
+    <JogosTable :jogosLista ="this.jogosLista"/>
+  </v-container>
+</template>
+
+<script>
+import JogosTable from './components/JogosTable'; 
+export default {
+  name: 'ListaJogos',
+  components: {
+    JogosTable,
+  },
+  data() {
+    return {
+      jogosLista: []
+    }
+  },
+  created() {
+    fetch('https://it3zxc-default-rtdb.firebaseio.com/lazer/jogos.json')
+      .then(response => response.json())
+      .then(json => {
+        this.jogosLista = json;
+        console.log(this.jogosLista);
+      });
+  },
+}
+</script>
--- src/router/index.js
+++ src/router/index.js
@@ -1,7 +1,7 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from '../views/Home.vue'
-import About from '../views/About.vue'
+import About from '../views/JogosList.vue'
 
 Vue.use(VueRouter)
 
@@ -11,8 +11,8 @@ const routes = [
     component: Home
   },
   {
-    path: '/about',
-    component: About
+    path: '/jogoslist',
+    component: JogosList
   }
 ]
 
