a29a86093da70f6987f678ce96eb355e1937ee73
Thiago Caldeira da Silva <thiago.caldeiraa@gmail.com>
2021-10-02 15:34:41 -0300

    Teste_Branch
    teste3

--- src/router/index.js
+++ src/router/index.js
@@ -1,7 +1,7 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from '../views/Home.vue'
-import About from '../views/JogosList.vue'
+import About from '../views/About.vue'
 
 Vue.use(VueRouter)
 
@@ -11,8 +11,8 @@ const routes = [
     component: Home
   },
   {
-    path: '/jogoslist',
-    component: JogosList
+    path: '/about',
+    component: About
   }
 ]
 
