3a1c94a4fb476375fa17b41d9edacd86190e057a
Thiago Caldeira da Silva <thiago.caldeiraa@gmail.com>
2021-10-02 16:45:16 -0300

    JogosTable
    Alterações de código. "JogosTable.Vue", "index _.js" e "JogosList.vue"

---
+++ src/components/JogosTable.vue
@@ -0,0 +1,99 @@
+<template>
+
+  <v-simple-table dark>
+
+    <template v-slot:default>
+
+      <thead>
+
+        <tr>
+
+          <th class="text-left">
+
+            Genero
+
+          </th>
+
+          <th class="text-left">
+
+            ID
+
+          </th>
+
+          <th class="text-left">
+
+            Nome
+
+          </th>
+
+          <th class="text-left">
+
+            Plataforma
+
+          </th>
+
+          <th class="text-right">
+
+            Ano
+
+          </th>
+
+        </tr>
+
+      </thead>
+
+      <tbody>
+
+        <tr
+
+          v-for="jogos in jogosLista"
+
+          :key="jogos.id"
+
+        >
+
+          <td><img :src="jogos.photo" :alt ="jogos.name"/></td>
+
+          <td>{{ jogos.genre }}</td>
+
+          <td>{{ jogos.name }}</td>
+
+          <td>{{ jogos.plataform }}</td>
+
+          <td>{{ jogos.id }}</td>
+          
+          <td>{{ jogos.year }}</td>
+
+        </tr>
+
+      </tbody>
+
+    </template>
+
+  </v-simple-table>
+
+</template>
+
+
+
+<script>
+
+export default {
+
+  name: 'JogosTable',
+
+  props: {
+
+    jogosLista: {
+
+      type: Array,
+
+      required: true
+
+    }
+
+  }
+
+}
+
+</script>
